2ae867599904295f2450b59a1c2cb2748812ccbe
DonPingo <DonPingo@DESKTOP-EKSUKV2>
2023-06-26 10:32:00 +0200

    El pepe

--- src/Ejercicios04/Ejercicio_07.java
+++ src/Ejercicios04/Ejercicio_07.java
@@ -3,8 +3,9 @@ package Ejercicios04;
 public class Ejercicio_07 {
 
 	public static void main(String[] args) {
-		// TODO Esbozo de método generado automáticamente
-
+		// 7. Dados dos arrays de reales de tamaño N, suponiendo que modelan dos vectores de un 
+		//espacio N-dimensional, decir si son paralelos o no. Son paralelos si todos sus valores 
+		//son proporcionales
 	}
 
 }
